c7a7a3dd8f9771becca075412d6a755ba84c53a1
Armando Alves Neto <64908110+aaneto2@users.noreply.github.com>
2026-03-23 11:00:19 -0300

    Add files via upload

--- aula03/class_gridworld_env.py
+++ aula03/class_gridworld_env.py
@@ -28,7 +28,12 @@ class Gridworld_Env:
         self.size = size
 
         # acoes
-        self.actions = [np.array([np.cos(th), np.sin(th)]) for th in np.linspace(0, 2.0*np.pi, 5)[:-1]]
+        self.actions = [
+                           np.array([-1,  0]),  # up
+                           np.array([ 0,  1]),   # right
+                           np.array([ 1,  0]),   # down
+                           np.array([ 0, -1])   # left
+                       ]
 
         # inicia o mundo
         self.reset()
@@ -42,11 +47,13 @@ class Gridworld_Env:
     # reseta o grid
     def reset(self):
         # estados terminais
-        self.terminais = [np.array([0,0]), np.array([self.size-1, self.size-1])]
+        self.terminal_states = {(0, 0), (self.size-1, self.size-1)}
         
         # retorna estado aleatório
         state = np.random.choice(self.size, 2)
-        return tuple(state[:])
+
+        self.state = tuple(state)
+        return self.state
 
     ##########################################
     # step
@@ -62,21 +69,11 @@ class Gridworld_Env:
         # reward
         reward = self.getReward()
 
-        # outros
-        done = False
-        info = {}
-
         # eh um estado final?
-        for t in self.terminais:
-            if  np.array_equal(state, t):
-                next_state = state
-                reward = 0.0
-                done = True
-
-        next_state = tuple(next_state[:])
-
-        # retorna
-        return next_state, reward, done, info
+        if state in self.terminal_states:
+            return tuple(state), 0.0, True, {'Terminou'}
+        else:
+            return tuple(next_state), reward, False, {}
 
     ##########################################
     # reforço
@@ -87,14 +84,15 @@ class Gridworld_Env:
     def render(self, value, pi=None):
 
         # Plota mapa de valor
-        fig = plt.subplots(figsize=(2*self.size, self.size))
+        if not (pi is None):
+            fig = plt.subplots(figsize=(2*self.size, self.size))
+            plt.subplot(1, 2, 1)
+        else:
+            fig = plt.subplots(figsize=(self.size, self.size))
 
         # funcao valor
-        plt.subplot(1, 2, 1)
+        sn.heatmap(value, annot=True, fmt=".1f", cmap='crest', linewidths=1, linecolor="black", cbar=False, square=True)
         plt.gca().set_title(r'$V(s)$')
-        W = LinearSegmentedColormap.from_list('w', ["w", "w"], N=256)
-        plt.gca().grid(which='major', axis='both', linestyle='-', color='k', linewidth=2)
-        sn.heatmap(value, annot=True, fmt=".1f", cmap='crest', linewidths=1, linecolor="black", cbar=False)
 
         # Plota mapa da politica
         if not (pi is None):
